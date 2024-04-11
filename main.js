5ff3dc8f284f0f572088747a9e402a2f188f8cae
yCamii <105953145+yCamii@users.noreply.github.com>
2024-04-11 19:52:25 -0300

    ss

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const botoes = document.querySelectorAll('.botao');
-const textos = document.querySelector('.aba-conteudo')
+const textos = document.querySelectorAll('.aba-conteudo')
 
 for (let i = 0; i < botoes.length; i++) {
     botoes[i].onclick = function () {
@@ -9,7 +9,7 @@ for (let i = 0; i < botoes.length; i++) {
             textos[j].classList.remove('ativo');
         }
         botoes[i].classList.add('ativo');
-       textos[i].classList.add('ativo');
+        textos[i].classList.add('ativo');
 
     }
 }
