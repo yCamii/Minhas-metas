fa19b936d49a717a01cc87e2a564a280a230487a
yCamii <105953145+yCamii@users.noreply.github.com>
2024-04-11 20:54:42 -0300

    kk

--- main.js
+++ main.js
@@ -16,4 +16,6 @@ for (let i = 0; i < botoes.length; i++) {
 
 const contadores = document.querySelectorAll('.contador');
 const tempoObjetivo1 = new Date('2025-04-04T00:00:00');
+let tempoAtual = new Date();
+
 contadores[0].textContent = tempoObjetivo1;
