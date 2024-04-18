d58e550306f7a7a70b0bf87c4a7f075026b5961b
yCamii <105953145+yCamii@users.noreply.github.com>
2024-04-18 20:36:13 -0300

    kl

--- main.js
+++ main.js
@@ -27,6 +27,10 @@ function calculaTempo(tempoObjetivo1) {
     let minutos = Math.floor(segundos / 60);
     let horas = Math.floor(minutos / 60);
     let dias = Math.floor(horas / 24);
+
+    segundos %= 60;
+    minutos %=60:
+    horas %=24
     return dias +'dias' + horas + 'horas' + minutos + 'minutos' + segundos + 'segundos';
 
 }
