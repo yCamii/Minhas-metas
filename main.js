9c6bd3eacb6480ded2cecf10608207e9d1bfaaee
yCamii <105953145+yCamii@users.noreply.github.com>
2024-04-18 20:39:58 -0300

    vghdsyufg

--- main.js
+++ main.js
@@ -31,7 +31,7 @@ function calculaTempo(tempoObjetivo1) {
     segundos %= 60;
     minutos %=60;
     horas %=24;
-    
+
     return dias +'dias' + horas + 'horas' + minutos + 'minutos' + segundos + 'segundos';
 
 }
