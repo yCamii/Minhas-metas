667ea69eb495544c86af5e159cb01279d9b8069b
yCamii <105953145+yCamii@users.noreply.github.com>
2024-05-02 20:28:21 -0300

    lkjj

--- main.js
+++ main.js
@@ -23,12 +23,17 @@ const tempoObjetivo4 = new Date('2026-01-01T00:00:00');
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
+function atualizaCronometro(){
 for (let i = 0; i < contadores.length; i++) {
     contadores[i].textContent = calculaTempo(tempos[i])
+}    
 }
+
+function comecaCronometro(){
 
 atualizaCronometro();
 setInterval(atualizaCronometro, 1000);
+}
 
 
 
