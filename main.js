be165fbe76376a1c4660a71cf83b5be688805aba
yCamii <105953145+yCamii@users.noreply.github.com>
2024-05-02 19:59:34 -0300

    hbhhb

--- main.js
+++ main.js
@@ -22,6 +22,9 @@ const tempoObjetivo4 = new Date('2025-01-01T00:00:00');
 
 
 contadores[0].textContent = calculaTempo(tempoObjetivo1);
+contadores[1].textContent = calculaTempo(tempoObjetivo2);
+contadores[2].textContent = calculaTempo(tempoObjetivo3);
+contadores[3].textContent = calculaTempo(tempoObjetivo4);
 
 function calculaTempo(tempoObjetivo1) {
     let tempoAtual = new Date();
