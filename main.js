df8831bd67cc3903ae5cb5cce980e90fe9bb83f7
yCamii <105953145+yCamii@users.noreply.github.com>
2024-05-02 20:32:00 -0300

    mlkjlk

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll('.contador');
-const tempoObjetivo1 = new Date('2010-03-24T00:00:00');
+const tempoObjetivo1 = new Date('2025-03-24T00:00:00');
 const tempoObjetivo2 = new Date('2025-01-01T00:00:00');
 const tempoObjetivo3 = new Date('2025-02-01T00:00:00');
 const tempoObjetivo4 = new Date('2026-01-01T00:00:00');
