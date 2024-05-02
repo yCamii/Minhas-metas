c8b6a10ab26849f887ab6c9188d402e6db52afcf
yCamii <105953145+yCamii@users.noreply.github.com>
2024-05-02 19:54:50 -0300

    mmm

--- main.js
+++ main.js
@@ -15,7 +15,10 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll('.contador');
-const tempoObjetivo1 = new Date('2025-04-04T00:00:00');
+const tempoObjetivo1 = new Date('2025-03-24T00:00:00');
+const tempoObjetivo2 = new Date('2025-01-01T00:00:00');
+const tempoObjetivo3 = new Date('2024-12-10T00:00:00');
+const tempoObjetivo4 = new Date('2025-01-01T00:00:00');
 
 
 contadores[0].textContent = calculaTempo(tempoObjetivo1);
